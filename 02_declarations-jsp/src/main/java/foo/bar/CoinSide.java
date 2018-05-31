04626df9a66e82208a1a17b78fe544e736095ab2
Tomek Owczarek <tomasz.owczarek@tomtom.com>
2018-05-31 14:36:58 +0200

    Solution for flip coin exercise

--- 02_declarations-jsp/src/main/java/foo/bar/CoinSide.java
+++ 02_declarations-jsp/src/main/java/foo/bar/CoinSide.java
@@ -1,12 +1,3 @@
-# Visit Counter 
-Example of declaration usage
-
-## How to run it?
-Follow the instructions mentioned in [hello-world-jsp example](../00_hello-world-jsp/README.md)
-
-## Exercise
-Create simulation of throwing coin using following enum:
-```
 package foo.bar;
 
 import java.util.Arrays;
@@ -29,6 +20,3 @@ public enum CoinSide {
         return VALUES.get(RANDOM.nextInt(SIZE));
     }
 }
-```
-
-
